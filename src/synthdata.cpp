9c4a61276227708eac4631054935146d94e69c50
jonsd <jonsd@d46cf337-b52f-0410-862d-fd96e6ae7743>
2011-05-03 11:59:55 +0000

    [1.45.23]
    git-svn-id: https://espeak.svn.sourceforge.net/svnroot/espeak/trunk@263 d46cf337-b52f-0410-862d-fd96e6ae7743

--- src/synthdata.cpp
+++ src/synthdata.cpp
@@ -35,7 +35,7 @@
 #include "translate.h"
 #include "wave.h"
 
-const char *version_string = "1.45.21  27.Apr.11";
+const char *version_string = "1.45.22  29.Apr.11";
 const int version_phdata  = 0x014500;
 
 int option_device_number = -1;
