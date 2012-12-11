0bd76a88fa3e0fb3c2ddaf5f1cea547e494d285f
Reece H. Dunn <msclrhd@gmail.com>
2012-12-11 11:43:08 +0000

    Add an Application.mk file to build for armeabi, armeabi-v7a and x86

---
+++ jni/Application.mk
@@ -0,0 +1,9 @@
+APP_OPTIM := release
+APP_PLATFORM := android-8
+
+# For use with ndk-rXm (MIPS NDK)
+#APP_ABI := mips
+
+# For use with ndk-rX-crystax (Crystax NDK)
+APP_TOOLCHAIN_VERSION := 4.6.3
+APP_ABI := armeabi armeabi-v7a x86
