437a660cda4ee9f91984ce67cb578a93cffa8770
Reece H. Dunn <msclrhd@gmail.com>
2012-12-14 16:07:11 +0000

    Rework the launcher icon so it is different to the eyes-free icon, using the eSpeak lips icon as the reference.

---
+++ android/mkicons.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rsvg -w 24 -h 24 icons/launcher.svg res/drawable-ldpi/icon.png
+rsvg -w 32 -h 32 icons/launcher.svg res/drawable-mdpi/icon.png
+rsvg -w 48 -h 48 icons/launcher.svg res/drawable-hdpi/icon.png
+rsvg -w 64 -h 64 icons/launcher.svg res/drawable-xhdpi/icon.png
