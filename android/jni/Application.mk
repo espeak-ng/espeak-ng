2c20fe7e33ba08c51f2ff7b9a18ffe5072dae829
Reece H. Dunn <msclrhd@gmail.com>
2012-12-20 20:23:24 +0000

    Application.mk: align the ':=' and remove commented out code

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,9 +1,4 @@
-APP_OPTIM := release
-APP_PLATFORM := android-8
-
-# For use with ndk-rXm (MIPS NDK)
-#APP_ABI := mips
-
-# For use with ndk-rX-crystax (Crystax NDK)
+APP_OPTIM             := release
+APP_PLATFORM          := android-8
 APP_TOOLCHAIN_VERSION := 4.6.3
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI               := armeabi armeabi-v7a x86
