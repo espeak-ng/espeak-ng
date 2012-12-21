0a37be30b25bb98b94faf2aa6b7f45c49f7ede11
Reece H. Dunn <msclrhd@gmail.com>
2012-12-21 00:47:50 +0000

    Build a version of libttsespeak.so for the MIPS architecture.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_OPTIM             := release
 APP_PLATFORM          := android-8
 APP_TOOLCHAIN_VERSION := 4.6.3
-APP_ABI               := armeabi armeabi-v7a x86
+APP_ABI               := mips armeabi armeabi-v7a x86
