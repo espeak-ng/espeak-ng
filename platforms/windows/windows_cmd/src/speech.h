4634aa43a55c5fbeb98e58960d6365bc2cb5ae23
jonsd <jonsd@d46cf337-b52f-0410-862d-fd96e6ae7743>
2013-03-18 03:13:22 +0000

    [1.47.01]
    git-svn-id: https://espeak.svn.sourceforge.net/svnroot/espeak/trunk@320 d46cf337-b52f-0410-862d-fd96e6ae7743

--- platforms/windows/windows_cmd/src/speech.h
+++ platforms/windows/windows_cmd/src/speech.h
@@ -30,6 +30,7 @@
 #define __WIN32__
 #define NEED_WCSTOF
 #define NEED_GETOPT
+#define USE_MBROLA_LIB
 
 #define PATHSEP '\\'
 #define USE_PORTAUDIO
@@ -37,7 +38,6 @@
 #define INCLUDE_KLATT
 #define INCLUDE_MBROLA
 #define INCLUDE_SONIC
-
 
 // will look for espeak_data directory here, and also in user's home directory
 #define PATH_ESPEAK_DATA  "/usr/share/espeak-data"
