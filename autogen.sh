47af3ade04359e25da8ca39018f89a3579b9af08
Reece H. Dunn <msclrhd@gmail.com>
2014-08-21 11:23:39 +0100

    autogen.sh: fix libtoolize detection on Mac OSX

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+LIBTOOLIZE=`which libtoolize`
+if ! test -f "$LIBTOOLIZE" ; then
+	# Mac OSX support ...
+	LIBTOOLIZE=`which glibtoolize`
+fi
+
 mkdir -p m4
 ln -sf README.md README
 ln -sf docs/ReleaseNotes.md NEWS
 
 aclocal -I m4 || exit 1
 
-libtoolize || exit 1
+${LIBTOOLIZE} || exit 1
 autoheader || exit 1
 automake --add-missing || exit 1
 autoconf || exit 1
