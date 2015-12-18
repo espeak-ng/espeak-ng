a47bf04657687f8608ec56c5a95b0dae0fe45816
Reece H. Dunn <msclrhd@gmail.com>
2015-12-18 07:20:34 +0000

    Add copyright notices to files that are missing them.

--- src/libespeak-ng/debug.h
+++ src/libespeak-ng/debug.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2007 to 2009 by Jonathan Duddington
+ * email: jonsd@users.sourceforge.net
+ * Copyright (C) 2015 Reece H. Dunn
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see: <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DEBUG_H
 #define DEBUG_H
 
