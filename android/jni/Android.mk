0a2034b9795032644a7448a3d3f5662f5f20a2f5
Reece H. Dunn <msclrhd@gmail.com>
2017-01-05 20:59:32 +0000

    android: support building the new espeak-ng C code.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS = -std=c++11
+LOCAL_CFLAGS = -std=c11
 
 # ucd-tools wide-character compatibility support:
 
@@ -11,28 +11,29 @@ UCDTOOLS_SRC_FILES := \
 
 LOCAL_SRC_FILES += $(UCDTOOLS_SRC_FILES)
 
-# eSpeak (minus command line apps and espeakedit)
-
 ESPEAK_SOURCES := \
-  src/compiledict.cpp \
-  src/debug.cpp \
-  src/dictionary.cpp \
-  src/intonation.cpp \
-  src/klatt.cpp \
-  src/numbers.cpp \
-  src/readclause.cpp \
-  src/phonemelist.cpp \
-  src/setlengths.cpp \
-  src/sonic.cpp \
-  src/speak_lib.cpp \
-  src/synthdata.cpp \
-  src/synthesize.cpp \
-  src/synth_mbrola.cpp \
-  src/translate.cpp \
-  src/tr_languages.cpp \
-  src/voices.cpp \
-  src/wavegen.cpp \
-  src/wave.cpp
+  src/libespeak-ng/compiledata.c \
+  src/libespeak-ng/compiledict.c \
+  src/libespeak-ng/compilembrola.c \
+  src/libespeak-ng/dictionary.c \
+  src/libespeak-ng/error.c \
+  src/libespeak-ng/espeak_api.c \
+  src/libespeak-ng/ieee80.c \
+  src/libespeak-ng/intonation.c \
+  src/libespeak-ng/klatt.c \
+  src/libespeak-ng/numbers.c \
+  src/libespeak-ng/phonemelist.c \
+  src/libespeak-ng/readclause.c \
+  src/libespeak-ng/setlengths.c \
+  src/libespeak-ng/spect.c \
+  src/libespeak-ng/speech.c \
+  src/libespeak-ng/synthdata.c \
+  src/libespeak-ng/synthesize.c \
+  src/libespeak-ng/synth_mbrola.c \
+  src/libespeak-ng/translate.c \
+  src/libespeak-ng/tr_languages.c \
+  src/libespeak-ng/voices.c \
+  src/libespeak-ng/wavegen.c
 
 ESPEAK_SRC_PATH  := ../../src
 ESPEAK_SRC_FILES := \
@@ -52,7 +53,7 @@ LOCAL_SRC_FILES += \
 LOCAL_C_INCLUDES += \
   $(LOCAL_PATH)/include \
   $(LOCAL_PATH)/$(UCDTOOLS_SRC_PATH)/include \
-  $(LOCAL_PATH)/$(ESPEAK_SRC_PATH)
+  $(LOCAL_PATH)/$(ESPEAK_SRC_PATH)/include
 
 LOCAL_LDLIBS := \
   -llog
