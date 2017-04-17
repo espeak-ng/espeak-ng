9ef03b8ac89de9e5d9f5b7b05530482fcff63aa5
Reece H. Dunn <msclrhd@gmail.com>
2017-04-17 15:14:02 +0100

    ucd-tools: Comma eSpeakNG extended property support.

--- src/ucd-tools/src/include/ucd/ucd.h
+++ src/ucd-tools/src/include/ucd/ucd.h
@@ -363,6 +363,7 @@ static const ucd_property UCD_PROPERTY_PATTERN_SYNTAX                     = 0x00
 static const ucd_property UCD_PROPERTY_PREPENDED_CONCATENATION_MARK       = 0x0000000100000000ull; /**< @brief Prepended_Concatenation_Mark */
 
 // eSpeak NG extended properties:
+static const ucd_property ESPEAKNG_PROPERTY_COMMA                         = 0x1000000000000000ull; /**< @brief Comma */
 static const ucd_property ESPEAKNG_PROPERTY_EXCLAMATION_MARK              = 0x2000000000000000ull; /**< @brief Exclamation_Mark */
 static const ucd_property ESPEAKNG_PROPERTY_QUESTION_MARK                 = 0x4000000000000000ull; /**< @brief Question_Mark */
 static const ucd_property ESPEAKNG_PROPERTY_FULL_STOP                     = 0x8000000000000000ull; /**< @brief Full_Stop */
