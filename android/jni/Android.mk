58a036376296864d58337401817ab6f209ab6e86
Reece H. Dunn <msclrhd@gmail.com>
2017-07-06 19:29:14 +0100

    Android: Fix the location of ucd-tools (it is now in src/ucd-tools).

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_CFLAGS = -std=c11
 
 # ucd-tools wide-character compatibility support:
 
-UCDTOOLS_SRC_PATH  := ../../ucd-tools/src
+UCDTOOLS_SRC_PATH  := ../../src/ucd-tools/src
 UCDTOOLS_SRC_FILES := \
   $(subst $(LOCAL_PATH)/$(UCDTOOLS_SRC_PATH),$(UCDTOOLS_SRC_PATH),$(wildcard $(LOCAL_PATH)/$(UCDTOOLS_SRC_PATH)/*.c*))
 
