255147ee75abbbb61eb082ccca4f2322646da11c
Reece H. Dunn <msclrhd@gmail.com>
2017-10-11 07:26:35 +0100

    readclause.c: Fix -Wmissing-prototypes warnings.

--- src/libespeak-ng/readclause.c
+++ src/libespeak-ng/readclause.c
@@ -918,7 +918,7 @@ static int attr_prosody_value(int param_type, const wchar_t *pw, int *value_out)
 	return sign;   // -1, 0, or 1
 }
 
-int AddNameData(const char *name, int wide)
+static int AddNameData(const char *name, int wide)
 {
 	// Add the name to the namedata and return its position
 	// (Used by the Windows SAPI wrapper)
