8768cba0ae2a6fe349d0c2c16a26c36e9d7b901d
Reece H. Dunn <msclrhd@gmail.com>
2017-10-11 07:29:49 +0100

    numbers.c: Fix -Wmissing-prototypes warnings.

--- src/libespeak-ng/numbers.c
+++ src/libespeak-ng/numbers.c
@@ -548,7 +548,7 @@ static const int number_ranges[] = {
 	0
 };
 
-int NonAsciiNumber(int letter)
+static int NonAsciiNumber(int letter)
 {
 	// Change non-ascii digit into ascii digit '0' to '9', (or -1 if not)
 	const int *p;
@@ -1711,7 +1711,7 @@ static int LookupNum3(Translator *tr, int value, char *ph_out, int suppress_null
 	return 0;
 }
 
-bool CheckThousandsGroup(char *word, int group_len)
+static bool CheckThousandsGroup(char *word, int group_len)
 {
 	// Is this a group of 3 digits which looks like a thousands group?
 	int ix;
