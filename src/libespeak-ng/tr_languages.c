d9cb351b199ef553334b09433900303d9a93dcb3
Rongcui Dong <rongcuid@outlook.com>
2019-04-09 17:32:17 -0400

    Revert

--- src/libespeak-ng/tr_languages.c
+++ src/libespeak-ng/tr_languages.c
@@ -1561,8 +1561,8 @@ Translator *SelectTranslator(const char *name)
 		tr->langopts.ideographs = 1;
 		tr->langopts.our_alphabet = 0x3100;
 		tr->langopts.word_gap = 0x21; // length of a final vowel is less dependent on the next consonant, don't merge consonant with next word
-		tr->langopts.listx = 1; // compile zh_listx after zh_list
 		if (name2 == L3('z', 'h', 'y')) {
+			tr->langopts.listx = 1; // compile zh_listx after zh_list
 			tr->langopts.textmode = true;
 			tr->langopts.numbers = 1;
 			tr->langopts.numbers2 = NUM2_ZERO_TENS;
