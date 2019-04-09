20967bc54ac33a49bf4bbd63a8cc77254e32013f
Rongcui Dong <rongcuid@outlook.com>
2019-04-09 15:43:46 -0400

    Always compile zh-listx after list. Otherwise overwritten

--- src/libespeak-ng/tr_languages.c
+++ src/libespeak-ng/tr_languages.c
@@ -1561,9 +1561,9 @@ Translator *SelectTranslator(const char *name)
 		tr->langopts.ideographs = 1;
 		tr->langopts.our_alphabet = 0x3100;
 		tr->langopts.word_gap = 0x21; // length of a final vowel is less dependent on the next consonant, don't merge consonant with next word
+		tr->langopts.listx = 1; // compile zh_listx after zh_list
 		if (name2 == L3('z', 'h', 'y')) {
 			tr->langopts.textmode = true;
-			tr->langopts.listx = 1; // compile zh_listx after zh_list
 			tr->langopts.numbers = 1;
 			tr->langopts.numbers2 = NUM2_ZERO_TENS;
 			tr->langopts.break_numbers = BREAK_INDIVIDUAL;
