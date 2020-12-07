9303e17a48b507931570246865a44c397917d68a
datajake1999 <datajake@gmail.com>
2020-12-07 02:52:27 -0800

    Fixed speechPlayer licensing issue.

--- src/speechPlayer/src/sample.h
+++ src/speechPlayer/src/sample.h
@@ -3,13 +3,14 @@ This file is a part of the NV Speech Player project.
 URL: https://bitbucket.org/nvaccess/speechplayer
 Copyright 2014 NV Access Limited.
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2.0, as published by
-the Free Software Foundation.
+it under the terms of the GNU General Public License, as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 This license can be found at:
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+http://www.gnu.org/licenses/gpl.html
 */
 
 #ifndef SPEECHPLAYER_SAMPLE_H
