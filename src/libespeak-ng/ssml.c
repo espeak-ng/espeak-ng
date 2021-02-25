19e2f697f63a20911e65f7363a2458e733fdd937
Juho Hiltunen <jaacoppi@kapsi.fi>
2021-02-25 19:30:08 +0200

    code cleanup: remove unnecessary current_voice_id in readclause.c
    Instead of passing a temporary variable current_voice_id to SSML
    processing logic and then copying the value to voice_change, pass
    voice_change directly. GetVoiceAttributes() both sets voice_change and
    retuns CLAUSE_TYPE_VOICE_CHANGE that is used by TranslateClause() for
    error checking.
    
    tests/ssml/language-switch.ssml covers the intended behavior of language
    switching and will fail when either voice_change or CLAUSE_TYPE_VOICE_CHANGE
    is not set correctly.

--- src/libespeak-ng/ssml.c
+++ src/libespeak-ng/ssml.c
@@ -312,7 +312,7 @@ static int GetVoiceAttributes(wchar_t *pw, int tag_type, SSML_STACK *ssml_sp, SS
 	// a voice change.
 	// If it's a closing tag, delete the top frame of the stack and determine whether this implies
 	// a voice change.
-	// Returns  CLAUSE_TYPE_VOICE_CHANGE if there is a voice change
+	// if there is a voice change, sets current_voice_id and returns CLAUSE_TYPE_VOICE_CHANGE
 
 	wchar_t *lang;
 	wchar_t *gender;
