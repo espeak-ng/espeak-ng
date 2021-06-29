2fb981855b9be2e001801be9ec368749d66e637c
agonzalezd <agonzalezd@vicomtech.org>
2021-06-29 11:40:40 +0200

    eu: Updated Basque phonetics and stress rule

--- src/libespeak-ng/tr_languages.c
+++ src/libespeak-ng/tr_languages.c
@@ -762,8 +762,8 @@ Translator *SelectTranslator(const char *name)
 		static const short stress_lengths_eu[8] = { 200, 200,  200, 200,  0, 0,  210, 230 }; // very weak stress
 		static const unsigned char stress_amps_eu[8] = { 16, 16, 18, 18, 18, 18, 18, 18 };
 		SetupTranslator(tr, stress_lengths_eu, stress_amps_eu);
-		tr->langopts.stress_rule = STRESSPOSN_2L; // ?? second syllable, but not on a word-final vowel
-		tr->langopts.stress_flags = S_FINAL_VOWEL_UNSTRESSED;
+		tr->langopts.stress_rule = 15; 
+		tr->langopts.stress_flags = S_FINAL_VOWEL_UNSTRESSED | S_MID_DIM;
 		tr->langopts.param[LOPT_SUFFIX] = 1;
 		tr->langopts.numbers = NUM_SINGLE_STRESS | NUM_DECIMAL_COMMA | NUM_HUNDRED_AND | NUM_OMIT_1_HUNDRED | NUM_OMIT_1_THOUSAND | NUM_VIGESIMAL;
 	}
