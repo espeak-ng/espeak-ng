e8f91839f1f28137a5610c251e69c21c4f8b3473
Yury Popov <dev@syncleo-iot.com>
2022-11-19 21:38:21 +0300

    ci: cmake-based

---
+++ cmake/package.cmake
@@ -0,0 +1,17 @@
+list(APPEND CPACK_SOURCE_IGNORE_FILES
+  /\.git/
+  /\.gitignore
+  /\.github/
+  /\.tx/
+  /\.travis.yml
+  /_layouts/
+  /android/
+  /build/
+  /chromium_extension/
+  /data/
+  /docs/
+  /emscripten/
+  /fastlane/
+  /tools/
+  /vim/
+)
