ec0969385d0cd04088c6900d7d9c392fa2126c24
Patryk Miś <foss@patrykmis.com>
2023-11-15 18:16:31 +0100

    Android: add `.dev` suffix to app id for debug builds
    This makes possible for debug and release builds to coexist on the same device.
    
    Signed-off-by: Patryk Miś <foss@patrykmis.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -69,6 +69,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".dev"
+        }
         release {
             isMinifyEnabled = false
             isShrinkResources = false
