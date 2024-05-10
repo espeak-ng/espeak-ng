2f7baf70e221e8276189d566a507d357207de711
Andras Eliassen <andras@isolveit.net>
2024-05-10 17:08:30 +0200

    added basic support for faroese

--- src/libespeak-ng/tr_languages.c
+++ src/libespeak-ng/tr_languages.c
@@ -826,6 +826,17 @@ Translator *SelectTranslator(const char *name)
 	case L('e', 't'): // Estonian
 		tr->encoding = ESPEAKNG_ENCODING_ISO_8859_4;
 		// fallthrough:
+	case L('f', 'o'): // Faroese
+	{
+		//static const short stress_lengths_da[8] = { 160, 140, 200, 200, 0, 0, 220, 230 };
+		//SetupTranslator(tr, stress_lengths_da, NULL);
+
+		//tr->langopts.stress_rule = STRESSPOSN_1L;
+		//tr->langopts.param[LOPT_PREFIXES] = 1;
+		//SetLetterVowel(tr, 'y');
+		tr->langopts.numbers = NUM_DECIMAL_COMMA | NUM_SWAP_TENS | NUM_HUNDRED_AND | NUM_OMIT_1_HUNDRED | NUM_ORDINAL_DOT | NUM_1900 | NUM_ROMAN | NUM_ROMAN_CAPITALS | NUM_ROMAN_ORDINAL;
+	}
+		break;
 	case L('f', 'i'): // Finnish
 	{
 		tr->langopts.long_stop = 130;
