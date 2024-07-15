58e453c6c487ac5fc42857d5087a2f7b9fd3fd4e
Carme Armentano <carme.armentano@gmail.com>
2024-07-15 16:51:43 +0200

    no valencian ellisions

--- src/libespeak-ng/tr_languages.c
+++ src/libespeak-ng/tr_languages.c
@@ -744,7 +744,7 @@ Translator *SelectTranslator(const char *name)
 	case L('a', 'n'): // Aragonese
 	case L('c', 'a'): // Catalan
 	{
-        static const wchar_t ca_punct_within_word[] = { '\'', 0xb7, 0 }; // ca: allow middle-dot within word
+        static const wchar_t ca_punct_within_word[] = { '\'', 0xb7, 0}; // ca: allow middle-dot within word
 
         tr->langopts.numbers = NUM_SINGLE_STRESS | NUM_DECIMAL_COMMA | NUM_AND_UNITS | NUM_OMIT_1_HUNDRED | NUM_OMIT_1_THOUSAND | NUM_ROMAN | NUM_ROMAN_AFTER | NUM_DFRACTION_4;
 		tr->langopts.numbers2 = NUM2_MULTIPLE_ORDINAL | NUM2_ORDINAL_NO_AND;
