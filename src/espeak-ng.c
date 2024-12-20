espeak-ng/espeak-ng

commit eaa0c9aa08350ce8a1343aed4c0a87c1cc329b0e

Fix truncated fgetc return value in main(espeak-ng.c). [Coverity]

src/espeak-ng.c
@@ -672,8 +672,10 @@ int main(int argc, char **argv)
 		} else {
 			// bulk input on stdin
 			ix = 0;
-			while (!feof(stdin)) {
-				p_text[ix++] = fgetc(stdin);
+			while (true) {
+				if ((c = fgetc(stdin)) == EOF)
+					break;
+				p_text[ix++] = (char)c;
 				if (ix >= (max-1)) {
 					max += 1000;
 					char *new_text = (char *)realloc(p_text, max);

commit 33b6daf420710eca2f6b2dcdc8eb3f6d72e1d313

Fix dropping last byte of stdin input
This got broken with eaa0c9aa08350ce8a1343aed4c0a87c1cc329b0e ("Fix
truncated fgetc return value in main(espeak-ng.c). [Coverity]") which
rightfully stopped adding a bogus EOF at the end of the input, which
this line was probably dropping just by luck.

Fixes brailcom/speechd#978

src/espeak-ng.c
@@ -762,8 +762,8 @@ int main(int argc, char **argv)
 				}
 			}
 			if (ix > 0) {
-				p_text[ix-1] = 0;
-				espeak_Synth(p_text, ix+1, 0, POS_CHARACTER, 0, synth_flags, NULL, NULL);
+				p_text[ix] = 0;
+				espeak_Synth(p_text, ix, 0, POS_CHARACTER, 0, synth_flags, NULL, NULL);
 			}
 		}
 