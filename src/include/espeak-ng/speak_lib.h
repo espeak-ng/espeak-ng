44dbd41630372ad4aed0fc348fdb38cabac3c4a1
Jonathan McDowell <noodles@earth.li>
2025-01-12 19:14:31 +0000

    Add espeak_TextToPhonemesWithTerminator
    This variant of espeak_TextToPhonemes also returns the clause terminator
    for the text.

--- src/include/espeak-ng/speak_lib.h
+++ src/include/espeak-ng/speak_lib.h
@@ -544,6 +544,12 @@ ESPEAK_API const char *espeak_TextToPhonemes(const void **textptr, int textmode,
 #ifdef __cplusplus
 extern "C"
 #endif
+ESPEAK_API const char *espeak_TextToPhonemesWithTerminator(const void **textptr, int textmode, int phonememode, int *terminator);
+/* Version of espeak_TextToPhonemes that also returns the clause terminator (e.g., CLAUSE_INTONATION_FULL_STOP) */
+
+#ifdef __cplusplus
+extern "C"
+#endif
 ESPEAK_API void espeak_CompileDictionary(const char *path, FILE *log, int flags);
 /* Compile pronunciation dictionary for a language which corresponds to the currently
    selected voice.  The required voice should be selected before calling this function.
