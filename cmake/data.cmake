e01bf581d47dd2b5b5ec72f95b574d6b1d1944a1
Theodore Dubois <tblodt@icloud.com>
2025-08-20 09:30:18 -0700

    tok: Add barebones voice for Toki Pona
    Lots of things to be improved here, but the voice is understandable.

--- cmake/data.cmake
+++ cmake/data.cmake
@@ -18,7 +18,7 @@ list(APPEND _dict_compile_list
   qdb quc qu qya
   ro ru rup
   sd shn si sjn sk sl smj sq sr sv sw
-  ta te ti th tk tn tr tt
+  ta te ti th tk tn tok tr tt
   ug uk ur uz
   vi
   yue
